92daf01b17fa126ef0703b70d1c685b6c685bb95
Andreas-Lopez <andreas.lopez@edu.univ-fcomte.fr>
2022-01-05 10:22:27 +0100

    carousel dans la page autres projets fini

--- src/js/fonctions.js
+++ src/js/fonctions.js
@@ -1,6 +1,6 @@
 function carrouselAlternate() {
 
-    let images = document.querySelectorAll('.image15');
+    let images = document.querySelectorAll('.carousel__item');
     let d = 2000;
     let delta = 1000;
 
@@ -9,14 +9,23 @@ function carrouselAlternate() {
     });
     
     let anim15 = anime.timeline({
-        targets: '.imgalt',
+        targets: '.carousel__item',
         loop: true,
         direction: 'alternate',
         easing: 'linear',
         delay: function(img, ind) {
             if (ind == 0) return delta;
             if (ind == 1) return 2*delta + d;
-            return 3*delta + 2*d;
+            if (ind == 2) return 3*delta + d*2;
+            if (ind == 3) return 4*delta + d*3;
+            if (ind == 4) return 5*delta + d*4;
+            if (ind == 5) return 6*delta + d*5;
+            if (ind == 6) return 7*delta + d*6;
+            if (ind == 7) return 8*delta + d*7;
+            if (ind == 8) return 9*delta + d*8;
+            if (ind == 9) return 10*delta + d*9;
+            if (ind == 10) return 11*delta + d*10;
+            return 12*delta + 11*d;
         }
     });
     anim15.add({
